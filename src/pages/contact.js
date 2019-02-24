87fd9e77cce8119f49bc006ff4f7c719dc7823f8
Kevin Lim <kevin.lim.code@gmail.com>
2019-02-24 23:16:21 +1100

    modified a few files and set the templates to start working

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <p>This is the Contact Us page!</p>
+    )
+  }
+}
+
+export default Contact
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <p>This is the Home page!</p>
+    )
+  }
+}
+
+export default Home
