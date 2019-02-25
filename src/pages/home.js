c55590e9632b33c61cfe00747db35b354781f7ef
Kevin Lim <kevin.lim.code@gmail.com>
2019-02-25 23:33:50 +1100

    began on the home pg

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import NavBar from '../components/navbar.js';
+import mainBG from '../img/home/img/Landing6.png';
 
 class Home extends Component {
   render() {
     return (
-      <p>This is the Home page!</p>
+      <div>
+        <NavBar/>
+        <img src={mainBG} alt={'Background Image'} className={'mainBG'}/>
+        <p>This is the Home page!</p>
+      </div>
     )
   }
 }
