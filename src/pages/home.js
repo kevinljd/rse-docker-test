ab144bade27a729ad2795ec6a5db10c74a8b558a
Kevin Lim <kevin.lim.code@gmail.com>
2019-02-26 09:36:00 +1100

    created headings on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import NavBar from '../components/navbar.js';
-import mainBG from '../img/home/img/Landing6.png';
+// import mainBG from '../img/home/img/Landing6.png';
+// import styled  from 'styled-components';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <NavBar/>
-        <img src={mainBG} alt={'Background Image'} className={'mainBG'}/>
-        <p>This is the Home page!</p>
-      </div>
+      <body>
+        <header>
+          <NavBar/>
+          <div>
+            <h1>Bridging the gaps between<br/>Classrooms and Workplaces</h1>
+            <a className={'scroll-to-start'} href={'#'}>Learn More</a>
+          </div>
+        </header>
+
+      </body>
     )
   }
 }
+
 
 export default Home
