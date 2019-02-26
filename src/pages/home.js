823ca8977bf1fed3f1e16a9f5e6e10d546f61dca
Kevin Lim <kevin.lim.code@gmail.com>
2019-02-26 14:41:21 +1100

    footer completed, fonts added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from '../components/navbar.js';
+import Footer from '../components/footer.js';
 // import mainBG from '../img/home/img/Landing6.png';
 // import styled  from 'styled-components';
 import './home.css'
@@ -10,10 +11,14 @@ class Home extends Component {
       <body>
         <NavBar/>
         <div className='header'>
-            <h1 className='mainHeading'>Bridging the gaps between<br/>Classrooms and Workplaces</h1>
-            <a className='scroll-to-start' href={'#'}>Learn More</a>
+          <h1 className='mainHeading'>Bridging the gaps between<br/>Classrooms and Workplaces</h1>
+          <a className='scroll-to-start' href={'#'}>Learn More</a>
         </div>
+        <div className='theProblem'>
+          <h1 className='mainHeading' style={{color: 'black'}}>The Problem</h1>
 
+        </div>
+        <Footer/>
       </body>
     )
   }
