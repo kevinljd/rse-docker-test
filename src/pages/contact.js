307d5f9b5d8301a6b01b7ce6a81e43ab7a6e893f
Celine <z5116868@unsw.edu.au>
2019-02-26 18:08:18 +1100

    added navbar and RSEcrop image to contact us page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+//import styled from 'styled-components'
+import { ImageBackground } from 'react-native'
+import NavBar from "../components/navbar"
+import RSEcrop from "../img/contact-us/img/RSEcrop.jpg"
+import FBLogo from "../img/contact-us/img/facebook-logo.png"
+import Handshake from "../img/contact-us/img/handshake.png"
+import Programs from "../img/contact-us/img/Programs.png"
 
+// width = {1422}
+// height = {700}
+console.log(RSEcrop);
 class Contact extends Component {
   render() {
     return (
-      <p>This is the Contact Us page!</p>
+      <body>
+        <NavBar/>
+        <img src = {RSEcrop} alt = "RSEcrop"
+        width = {1422}
+        height = {700} 
+        />
+
+      <p>Contact Us</p>
+
+      </body>
     )
   }
 }
