918abf3cb6e99afc479fcd1d7777fa8fe24543d9
Kevin Lim <kevin.lim.code@gmail.com>
2019-02-26 13:45:48 +1100

    fixed bg

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import NavBar from '../components/navbar.js';
 // import mainBG from '../img/home/img/Landing6.png';
 // import styled  from 'styled-components';
+import './home.css'
 
 class Home extends Component {
   render() {
     return (
       <body>
         <NavBar/>
-        <header>
+        <div className='header'>
           <div>
-            <h1>Bridging the gaps between<br/>Classrooms and Workplaces</h1>
-            <a className={'scroll-to-start'} href={'#'}>Learn More</a>
+            <h1 className='mainHeading'>Bridging the gaps between<br/>Classrooms and Workplaces</h1>
+            <a className='scroll-to-start' href={'#'}>Learn More</a>
           </div>
-        </header>
+        </div>
 
       </body>
     )
