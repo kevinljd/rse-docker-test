9ef2d81e437d2783728e36486f8a099d8dc15621
Celine <z5116868@unsw.edu.au>
2019-02-27 00:06:48 +1100

    added text on photo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-//import styled from 'styled-components'
+import styled from 'styled-components'
 import { ImageBackground } from 'react-native'
 import NavBar from "../components/navbar"
 import RSEcrop from "../img/contact-us/img/RSEcrop.jpg"
@@ -7,20 +7,47 @@ import FBLogo from "../img/contact-us/img/facebook-logo.png"
 import Handshake from "../img/contact-us/img/handshake.png"
 import Programs from "../img/contact-us/img/Programs.png"
 
-// width = {1422}
-// height = {700}
-console.log(RSEcrop);
 class Contact extends Component {
   render() {
     return (
       <body>
         <NavBar/>
-        <img src = {RSEcrop} alt = "RSEcrop"
-        width = {1422}
+        {/* <img src = {RSEcrop} alt = "RSEcrop"
+        width = {1424}
         height = {700} 
-        />
+        /> */}
+      <ImageBackground 
+        source = {require('../img/contact-us/img/RSEcrop.jpg')}
+        //resizeMode='contain'
+        // alignItems= 'stretch'
+        style={{ height:800}}
+        >
+        <Text
+          style = {{
+            fontFamily: 'Montserrat',
+            fontWeight: 'bold',
+            fontSize: 70,
+            textAlign: 'center',
+            marginTop: 400,
+            color: 'white',
+          }}
+        >
+          Got any further enquiries?
+        </Text>
+        <Text
+          style = {{
+            fontFamily: 'Montserrat',
+            fontSize: 70,
+            textAlign: 'center',
+            marginTop: 400,
+            color: 'white',
+          }}
+        >
+          Talk to us.
+        </Text>
+      </ImageBackground>
 
-      <p>Contact Us</p>
+      {/* <Title>Contact Us</Title> */}
 
       </body>
     )
@@ -28,3 +55,16 @@ class Contact extends Component {
 }
 
 export default Contact
+
+const Title = styled.h1 `
+  font-family: 'Montserrat';
+  font-weight: 'bold';
+  font-color: 'ffffff';
+  fontSize: 70;
+  textAlign: 'center'.
+  marginTop: 400;
+  color: 'white';
+`;
+const Text = styled.div `
+  font-family: 'Montserrat'; 
+`;
