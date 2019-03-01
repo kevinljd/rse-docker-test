100ff80af0c03d9d4b94e44e133ae4dc1046e245
Celine <z5116868@unsw.edu.au>
2019-03-02 00:29:23 +1100

    mostly finished except for adjusting images and fonts

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+// import styled  from 'styled-components';
+import './footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className={'footer'}>
+        <p>© 2018 REAL SKILLS EDUCATION. ALL RIGHTS RESERVED.</p>
+      </div>
+
+    )
+  }
+}
+
+
+
+export default Footer
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { ImageBackground } from 'react-native'
+import { ImageBackground, Text, View, StyleSheet } from 'react-native'
+
 import NavBar from "../components/navbar"
+import Footer from "../components/footer"
+
 import RSEcrop from "../img/contact-us/img/RSEcrop.jpg"
 import FBLogo from "../img/contact-us/img/facebook-logo.png"
 import Handshake from "../img/contact-us/img/handshake.png"
 import Programs from "../img/contact-us/img/Programs.png"
+
+require('typeface-montserrat')
+require('typeface-montserrat-alternates')
+require('typeface-roboto')
 
 class Contact extends Component {
   render() {
@@ -16,39 +23,45 @@ class Contact extends Component {
         width = {1424}
         height = {700} 
         /> */}
-      <ImageBackground 
-        source = {require('../img/contact-us/img/RSEcrop.jpg')}
-        //resizeMode='contain'
-        // alignItems= 'stretch'
-        style={{ height:800}}
-        >
-        <Text
-          style = {{
-            fontFamily: 'Montserrat',
-            fontWeight: 'bold',
-            fontSize: 70,
-            textAlign: 'center',
-            marginTop: 400,
-            color: 'white',
-          }}
-        >
-          Got any further enquiries?
-        </Text>
-        <Text
-          style = {{
-            fontFamily: 'Montserrat',
-            fontSize: 70,
-            textAlign: 'center',
-            marginTop: 400,
-            color: 'white',
-          }}
-        >
-          Talk to us.
-        </Text>
-      </ImageBackground>
+        <ImageBackground source = {require('../img/contact-us/img/RSEcrop.jpg')} style={{height:800}}>
+          <Text style = {{marginTop: 400}}>
+            <ImageTextBold>Got any further enquiries?</ImageTextBold>
+            <ImageText>Talk to us.</ImageText>
+          </Text>
+        </ImageBackground>
+          <ContactUs>Contact Us</ContactUs>
+          <Line/>
+            <View style={containerStyle.rowContainer}>
+              <View style = {containerStyle.container}>
+                {<img src = {Handshake} alt = "Handshake"width = {100} height = {100} paddingRight= {50}></img>}
+              </View>
+              <View style = {containerStyle.container}>
+              {<img src = {Programs} alt = "Programs"width = {100} height = {100}paddingRight= {50}></img>}
+              </View>
+              <View style = {containerStyle.container}>
+              {<img src = {FBLogo} alt = "FBLogo" width = {90} height = {90} paddingRight= {50}></img>}
+              </View>
+           </View>
 
-      {/* <Title>Contact Us</Title> */}
-
+           <View style={containerStyle.rowContainer}>
+              <View style = {containerStyle.container}>
+                <Enquiries>For Partnership Enquiries</Enquiries>
+                <ReachOut>Reach out to us at</ReachOut>
+                <Link>alliance@realskillseducation.com</Link>
+              </View>
+              <View style = {containerStyle.container}>
+                <Enquiries>For Program Enquiries</Enquiries>
+                <ReachOut>Reach out to us at</ReachOut>
+                <Link>program@realskillseducation.com</Link>
+              </View>
+              <View style = {containerStyle.container}>
+                <Enquiries>For General Enquiries</Enquiries>
+                <ReachOut>Send us a message</ReachOut>
+                <Link>Facebook</Link>
+              </View>
+           </View>
+           <Padding/>
+           <Footer/>
       </body>
     )
   }
@@ -56,15 +69,83 @@ class Contact extends Component {
 
 export default Contact
 
-const Title = styled.h1 `
+// const Title = styled.div `
+//   font-family: 'typeface-montserrat';
+//   font-weight: 'bold';
+//   font-color: 'ffffff';
+//   fontSize: 70;
+//   textAlign: 'center';
+//   marginTop: 400;
+//   color: white;
+// `
+const containerStyle = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  }
+}); 
+
+const ImageTextBold = styled.div`
+  font-family: Montserrat;
+  font-size: 4em;
+  font-weight: bold;
+  color: white;
+  text-align: center;
+`
+const ImageText = styled.div`
+  font-family: Montserrat;
+  font-size: 3em;
+  text-align: center;
+  color: white;
+`
+
+const ContactUs= styled.div`
+  font-family: 'Roboto';
+  font-size: 3em;
+  text-align: center;
+  color: '#3e3e3e';
+  padding: 2rem;
+`
+
+const Enquiries= styled.div`
+  font-family: 'Roboto';
+  font-size: 1.5em;
+  text-align: center;
+  color: '#3e3e3e';
+  padding: 0.5rem;
+`
+const ReachOut= styled.div`
   font-family: 'Montserrat';
-  font-weight: 'bold';
-  font-color: 'ffffff';
-  fontSize: 70;
-  textAlign: 'center'.
-  marginTop: 400;
-  color: 'white';
-`;
-const Text = styled.div `
-  font-family: 'Montserrat'; 
-`;
+  font-weight: 100;
+  font-size: 1em;
+  text-align: center;
+  color: '#3e3e3e';
+  padding: 0.5rem;
+`
+const Link= styled.div`
+  font-family: 'Roboto';
+  font-size: 1em;
+  font-weight: bold;
+  text-align: center;
+  color: '#a1a1a1';
+  padding: 0.5rem;
+`
+const Padding= styled.div`
+  padding: 10rem;
+`
+const Line = ({ color }) => (
+  <hr
+      style={{
+          color: '#eeeeee',
+          backgroundColor: '#eeeeee',
+          height: 2,
+          width: 1200,
+      }}
+  />
+);
+
