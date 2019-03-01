c7243605f66f2a50f362acb91dbf0aff101739f8
Celine <z5116868@unsw.edu.au>
2019-03-02 00:45:33 +1100

    finished but maybe been to change font colours

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,47 +19,38 @@ class Contact extends Component {
     return (
       <body>
         <NavBar/>
-        {/* <img src = {RSEcrop} alt = "RSEcrop"
-        width = {1424}
-        height = {700} 
-        /> */}
+        
         <ImageBackground source = {require('../img/contact-us/img/RSEcrop.jpg')} style={{height:800}}>
           <Text style = {{marginTop: 400}}>
             <ImageTextBold>Got any further enquiries?</ImageTextBold>
             <ImageText>Talk to us.</ImageText>
           </Text>
         </ImageBackground>
+
           <ContactUs>Contact Us</ContactUs>
           <Line/>
+
             <View style={containerStyle.rowContainer}>
               <View style = {containerStyle.container}>
                 {<img src = {Handshake} alt = "Handshake"width = {100} height = {100} paddingRight= {50}></img>}
-              </View>
-              <View style = {containerStyle.container}>
-              {<img src = {Programs} alt = "Programs"width = {100} height = {100}paddingRight= {50}></img>}
-              </View>
-              <View style = {containerStyle.container}>
-              {<img src = {FBLogo} alt = "FBLogo" width = {90} height = {90} paddingRight= {50}></img>}
-              </View>
-           </View>
-
-           <View style={containerStyle.rowContainer}>
-              <View style = {containerStyle.container}>
                 <Enquiries>For Partnership Enquiries</Enquiries>
                 <ReachOut>Reach out to us at</ReachOut>
                 <Link>alliance@realskillseducation.com</Link>
               </View>
               <View style = {containerStyle.container}>
+                {<img src = {Programs} alt = "Programs"width = {100} height = {100}paddingRight= {50}></img>}
                 <Enquiries>For Program Enquiries</Enquiries>
                 <ReachOut>Reach out to us at</ReachOut>
                 <Link>program@realskillseducation.com</Link>
               </View>
               <View style = {containerStyle.container}>
+                {<img src = {FBLogo} alt = "FBLogo" width = {90} height = {90} paddingRight= {50}></img>}
                 <Enquiries>For General Enquiries</Enquiries>
                 <ReachOut>Send us a message</ReachOut>
                 <Link>Facebook</Link>
               </View>
            </View>
+
            <Padding/>
            <Footer/>
       </body>
@@ -81,7 +72,7 @@ export default Contact
 const containerStyle = StyleSheet.create({
   container: {
     alignItems: 'center',
-    justifyContent: 'space-evenly',
+    padding: 20,
     
   },
   rowContainer: {
@@ -117,7 +108,7 @@ const Enquiries= styled.div`
   font-size: 1.5em;
   text-align: center;
   color: '#3e3e3e';
-  padding: 0.5rem;
+  padding: 1rem;
 `
 const ReachOut= styled.div`
   font-family: 'Montserrat';
@@ -133,7 +124,7 @@ const Link= styled.div`
   font-weight: bold;
   text-align: center;
   color: '#a1a1a1';
-  padding: 0.5rem;
+
 `
 const Padding= styled.div`
   padding: 10rem;
