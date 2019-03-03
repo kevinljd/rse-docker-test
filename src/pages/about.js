d1510aec10575e99640bd2f703e65cf5f94929b0
kath <ikath@hotmail.com>
2019-03-03 17:20:42 +1100

    added text need img to work

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,22 @@
 import './about.css';
+import { Row, Col} from 'react-grid-system'
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { Component } from 'react'
 
 class About extends Component {
   render() {
     return (
-      <div>
-          <h1 className={"title"}>Our Values</h1>
-          <div className={"row"}>
-            <div className={"col-sm-2"}></div>
-              <p><b>G - Give</b> before taking.
+      <div className={"container"}>
+          <h1 className={"title"} style={{fontSize:'4em', padding:'2em 0em'}}>About Us</h1>
+          <h1 className={"title"} style={{borderBottom: '2px solid black'}}>Values</h1>
+          <Row>
+            <Col sm={3} className={"text"}>
+              <img src={"./gift.png"} alt={"present"} />
+              <p><b style={{fontSize: '2em'}}>G</b> <br/>
+                <b>Give Before Taking</b> <br/><br/><br/>
                   Your true worth is determined by
                   how much you give in value than
-                  you take in payment.
+                  you take in payment. <br/>
                   This may sound like a recipe for
                   bankruptcy. But think again! Giving
                   is not a zero sum game. Remember
@@ -21,18 +25,61 @@ class About extends Component {
                   success.The more you give, the
                   more you have. You feel
                   empowered, capable, proud and
-                  overall great!
+                  overall great! <br/><br/>
                   At RSE we believe the same, and
                   look to create a community of
                   sharing and giving.
                   Generosity builds success.</p>
-            <div className={"col-sm-2"}></div>
-              <p>hello</p>
-            <div className={"col-sm-2"}></div>
-              <p>hello</p>
-            <div className={"col-sm-2"}></div>
-              <p>hello</p>
-          </div>
+            </Col>
+            <Col sm={3} className={"text"}>
+              <img src={"./Inspire.png"} alt={"light bulb"}/>
+              <p><b style={{fontSize: '2em'}}>I</b> <br/>
+              <b>Inspire Others To Suceed</b> <br/><br/><br/>
+              “Average people raise the bar on
+              themselves;<br/>
+              Good people raise the bar for
+              others;<br/>
+              Great people inspire others to
+              raise their own bar”<br/><br/>
+              Simply understanding the
+              importance of inspiration and making a conscious effort to
+              inspire is often all it takes to make
+              a tremendous difference for
+              others. When we look to develop
+              ourselves to become our very
+              best, we should also look to raise
+              those around us - we aspire to make a difference by supporting
+              those in our community to
+              achieve their goals.</p>
+            </Col>
+            <Col sm={3} className={"text"}>
+              <img src={"Fail.png"} alt={"X"}/>
+              <p><b style={{fontSize: '2em'}}>F</b> <br/>
+                <b>Fail, But Only Once</b> <br/><br/><br/>
+                  Instead of fearing failure,
+                  embrace it. But from each
+                  failure, learn what went
+                  wrong and aim to do it
+                  better the next time. At RSE,
+                  we believe that every failure
+                  is an invaluable learning
+                  experience which brings
+                  you closer to success.</p>
+            </Col>
+            <Col sm={3} className={"text"}>
+              <img src={"ice-cream.png"} alt={"X"}/>
+              <p><b style={{fontSize: '2em'}}>T</b> <br/>
+              <b>Try All The Flavours Of The Icecream</b> <br/><br/>
+              Try as many things as you
+              can and work out what
+              drives you. RSE provides
+              both staff and students
+              with opportunities they
+              wouldn't have otherwise,
+              allowing them to discover
+              their passion. </p>
+            </Col>
+          </Row>
       </div>
 
     )
