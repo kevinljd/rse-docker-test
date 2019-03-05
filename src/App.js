8d373c0e9ab5dc90dd4679ef884478991acd99a4
Celine <z5116868@unsw.edu.au>
2019-03-05 21:09:53 +1100

    finished everything except 2 missing picture

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { Switch, Route } from "react-router-dom";
 import Home from './pages/home';
 import About from './pages/about';
 import Contact from "./pages/contact";
+import Programs from "./pages/programs";
 import NavBar from "./components/navbar";
+
 
 
 class App extends Component {
@@ -14,7 +16,9 @@ class App extends Component {
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
           <Route exact path='/contact' component={Contact} />
+          <Route exact path='/programs' component={Programs} />
           <Route exact path='/nav' component={NavBar} />
+
         </Switch>
       </main>
     );
---
+++ src/pages/programs.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import { ImageBackground, Text, View, StyleSheet } from 'react-native'
+import { setConfiguration, Container, Row, Col } from 'react-grid-system'
+
+import NavBar from "../components/navbar"
+import Footer from "../components/footer"
+
+import Audience from "../img/programs/img/DSE_6807.jpg"
+
+require('typeface-montserrat')
+require('typeface-montserrat-alternates')
+require('typeface-roboto')
+
+
+class Programs extends Component {
+    render() {
+        return (
+            <body>
+            <NavBar/>
+
+            <ProgramsFont>PROGRAMS</ProgramsFont>
+
+            <Container>
+            <Row>
+              <Col sm={4} align="center">
+                <BlueBoxBold>RECRUITMENT</BlueBoxBold>
+                <BlueBox>ENRICHMENT PROGRAM</BlueBox>
+              </Col>
+              <Col sm={4} align="center">
+                <OrangeBoxBold>STEM LEADERS</OrangeBoxBold>
+                <OrangeBox>PROGRAM</OrangeBox>
+              </Col>
+              <Col sm={4} align="center">
+                <GrayBoxBold>FUTURE</GrayBoxBold>
+                <GrayBox>TECHNOLOGIES PROGRAM</GrayBox>
+              </Col>
+            </Row>
+          </Container>
+
+            <Container>
+            <Row>
+              <Col sm={4} align="center">
+              </Col>
+              <Col sm={4} align="center">
+                {<img src = {Audience} alt = "Audience" width = {475} height = {300}></img>}
+              </Col>
+              <Col sm={4} align="center">
+              </Col>
+            </Row>
+          </Container>
+
+          <Footer/>
+            </body>
+        )
+    }
+}
+
+export default Programs
+
+setConfiguration({ defaultScreenClass: 'sm', containerWidths: 1140 , gutterWidth: 0});
+
+const ProgramsFont = styled.div`
+    font-family: Montserrat;
+    font-size: 6em;
+    font-weight: bold;
+    color: black;
+    text-align: center;
+    padding: 3rem;
+`
+
+const BlueBoxBold = styled.div`
+    background-color: #10ccff;
+    font-family: Montserrat;
+    font-size: 3em;
+    font-weight: bold;
+    color: white;
+`
+const BlueBox = styled.div`
+    background-color: #10ccff;
+    font-family: Montserrat;
+    font-size: 2em;
+    color: white;
+`
+const OrangeBoxBold = styled.div`
+    background-color: #FF9000;
+    font-family: Montserrat;
+    font-size: 3em;
+    font-weight: bold;
+    color: white;
+`
+const OrangeBox = styled.div`
+    background-color: #FF9000;
+    font-family: Montserrat;
+    font-size: 2em;
+    color: white;
+`
+const GrayBoxBold = styled.div`
+    background-color: #3e3e3e;
+    font-family: Montserrat;
+    font-size: 3em;
+    font-weight: bold;
+    color: white;
+`
+const GrayBox = styled.div`
+    background-color: #3e3e3e;
+    font-family: Montserrat;
+    font-size: 2em;
+    color: white;
+`
