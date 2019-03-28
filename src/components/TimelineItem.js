e713b6c855fa5254525075aaeac6c1266a3582da
Kevin Lim <kevin.lim.code@gmail.com>
2019-03-29 01:59:02 +1100

    fixed up timeline.js TimelineItem.js files

---
+++ src/components/TimelineItem.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Row, Col } from 'react-grid-system'
+
+
+const TimelineItem = props => {
+  return (
+    <Row style={{margin: '1rem 0rem'}}>
+      <Col sm={12} lg={1}/>
+      <Col sm={12} lg={7}>
+        <h3>{props.title}</h3>
+        <p style ={{fontSize: '15px'}}>{props.body}</p>
+      </Col>
+      <Col sm={12} lg={3}>
+        <Icon src={props.circle.src} alt={props.circle.alt} />
+      </Col>
+
+
+      <Col sm={12} lg={1}/>
+
+    </Row>
+  )
+}
+
+const Photo = styled.img`
+  border: 10px solid white;
+  transform: translate(30px, -30px);
+  width: 10rem;
+  @media (max-width: 800px) {
+    display: none;
+  } 
+`
+
+const Icon = styled.img`
+    border-radius: 50%;
+    width: 8rem;
+    height: auto;
+    border: 2px solid white;
+    vertical-align: center;
+`
+
+export default TimelineItem
